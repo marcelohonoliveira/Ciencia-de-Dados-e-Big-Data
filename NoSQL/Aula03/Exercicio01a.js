11d7929776e37f55d3ee5f18ca0ccf50dd0e24f5
Marcelo Oliveira <marcelohonoliveira@gmail.com>
2016-12-04 17:33:31 -0200

    Add files via upload

---
+++ NoSQL/Aula03/Exercicio01a.js
@@ -0,0 +1,37 @@
+var map_fn = function()
+{
+    emit(this.text.substring(this.text.length - 2, this.text.length), 1);
+}
+
+var reduce_fn = function(key, values)
+{
+    return Array.sum(values);
+}
+
+var result = db.Vocabulary.mapReduce(
+	map_fn,
+	reduce_fn,
+	{
+		query: {text:  /((ar)|(er)|(ir)|(or)|(ur))$/},
+
+		out: "conta_palavras"
+	}
+
+);
+
+
+
+printjson(result);
+
+
+
+var cursor = db.conta_palavras.find({});
+while (cursor.hasNext())
+{
+	printjson(cursor.next())
+
+}
+
+
+cursor
+cursor.close();
